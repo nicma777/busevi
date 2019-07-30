01baff81ebafe711905c709bc3d72be0125ff565
nicma777 <manuel@tempusmedia.hr>
2019-07-30 16:04:28 +0200

    logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,28 +2,29 @@
 
 @section('content')
 
-    <div class="row header p-2 pr-2">
-        <div class="col-md-8">
-            <img src="http://www.gradski-parking.hr/tpl/stil/img/gradski_parking_sibenik_logo.png" alt="">
-        </div>
-        <div class="col-md-4 pr-2 second clock text-center">
-            {{ \Carbon\Carbon::now()->format('d.m.Y. H:i') }}
-        </div>
+<div class="row header p-2 pr-2">
+    <div class="col-md-8">
+        <img src="http://www.gradski-parking.hr/tpl/stil/img/gradski_parking_sibenik_logo.png" alt=""
+            style="width: 40%">
     </div>
-
-    <div class="row second mb-0">
-        <div class="col col-md-6  text-center text-white column-head">
-            DOLASCI &nbsp;&nbsp;<span class="text-second">ARIVALS</span>
-        </div>
-        <div class="col col-md-6 text-center text-white column-head">
-            POLASCI &nbsp;&nbsp;<span class="text-second">DEPARTURES</span>
-        </div>
+    <div class="col-md-4 pr-2 second clock text-center">
+        {{ \Carbon\Carbon::now()->format('d.m.Y. H:i') }}
     </div>
+</div>
 
-    <div class="row mt-0 pt-0 fill-height-or-more">
-        <div class="col col-md-6 p-0 ">
-            <table class="table table-zebra table-borderless ">
-                <thead>
+<div class="row second mb-0">
+    <div class="col col-md-6  text-center text-white column-head">
+        DOLASCI &nbsp;&nbsp;<span class="text-second">ARIVALS</span>
+    </div>
+    <div class="col col-md-6 text-center text-white column-head">
+        POLASCI &nbsp;&nbsp;<span class="text-second">DEPARTURES</span>
+    </div>
+</div>
+
+<div class="row mt-0 pt-0 fill-height-or-more">
+    <div class="col col-md-6 p-0 ">
+        <table class="table table-zebra table-borderless ">
+            <thead>
                 <tr>
                     <th>
                         <strong class="text-white header">Autobusna linija</strong>
@@ -41,30 +42,30 @@
                         <span class="text-second header">Arival</span>
                     </th>
                 </tr>
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 @foreach($arivals as $arival)
-                    <tr>
-                        <td>{{ $arival->city }}</td>
-                        <td>{{ $arival->carrier }}</td>
-                        <td>
-                            @if($arival->status == '1')
-                                <span  class="text-danger delay">DELAY</span>
-                            @elseif($arival->status == '2')
-                            <span  class="text-success ongate">ON GATE</span>
-                            @else
-                                <span>{{ \Carbon\Carbon::parse($arival->time)->format('H:i') }}</span>
-                            @endif
-                        </td>
+                <tr>
+                    <td>{{ $arival->city }}</td>
+                    <td>{{ $arival->carrier }}</td>
+                    <td>
+                        @if($arival->status == '1')
+                        <span class="text-danger delay">DELAY</span>
+                        @elseif($arival->status == '2')
+                        <span class="text-success ongate">ON GATE</span>
+                        @else
+                        <span>{{ \Carbon\Carbon::parse($arival->time)->format('H:i') }}</span>
+                        @endif
+                    </td>
 
-                    </tr>
+                </tr>
                 @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="col col-md-6  p-0">
-            <table class="table table-zebra table-borderless">
-                <thead>
+            </tbody>
+        </table>
+    </div>
+    <div class="col col-md-6  p-0">
+        <table class="table table-zebra table-borderless">
+            <thead>
                 <tr>
                     <th>
                         <strong class="text-white header">Autobusna linija</strong>
@@ -82,29 +83,29 @@
                         <span class="text-second header">Depatrure</span>
                     </th>
                 </tr>
-                </thead>
-                <tbody>
+            </thead>
+            <tbody>
                 @foreach($departures as $depatrure)
-                    <tr>
-                        <td>{{ $depatrure->city }}</td>
-                        <td>{{ $depatrure->carrier }}</td>
-                        <td>
-                            @if($depatrure->status)
-                                <strong  class="delay">DELAY</strong>
-                            @else
-                                <span>{{ \Carbon\Carbon::parse($depatrure->time)->format('H:i') }}</span>
-                            @endif
-                        </td>
+                <tr>
+                    <td>{{ $depatrure->city }}</td>
+                    <td>{{ $depatrure->carrier }}</td>
+                    <td>
+                        @if($depatrure->status)
+                        <strong class="delay">DELAY</strong>
+                        @else
+                        <span>{{ \Carbon\Carbon::parse($depatrure->time)->format('H:i') }}</span>
+                        @endif
+                    </td>
 
-                    </tr>
+                </tr>
                 @endforeach
 
-                </tbody>
-            </table>
-
-        </div>
-
+            </tbody>
+        </table>
 
     </div>
+
+
+</div>
 
 @endsection
